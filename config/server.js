0c1f3f613d358333c115b5c237a58313e42577e4
Derrick Mehaffy <derrickmehaffy@gmail.com>
2021-06-28 13:20:19 -0700

    project gen

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '9d717063b452b3e1082460084d77f88d'),
+    },
+  },
+});
